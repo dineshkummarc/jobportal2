c2a74bdfe40d2d8e260ff290d10ae4886f74c0ea
Dinesh Kumar <mapmydestination@gmail.com>
2025-04-17 21:39:23 +0530

    update

--- Job-Portal-In-PHP-Source-Code/jobportal/admin/logout.php
+++ Job-Portal-In-PHP-Source-Code/jobportal/admin/logout.php
@@ -1,24 +1,9 @@
-<?php 
-require_once '../include/initialize.php';
-// Four steps to closing a session
-// (i.e. logging out)
+<?php
+session_start();
 
-// 1. Find the session
-@session_start();
+	
+unset($_SESSION['name']);
+session_destroy();
 
-// 2. Unset all the session variables
-// unset( $_SESSION['USERID'] );
-// unset( $_SESSION['FULLNAME'] );
-// unset( $_SESSION['USERNAME'] );
-// unset( $_SESSION['PASS'] );
-// unset( $_SESSION['ROLE'] );
- 
-
-unset($_SESSION['ADMIN_USERID']);  
-unset($_SESSION['ADMIN_FULLNAME']); 
-unset($_SESSION['ADMIN_USERNAME']);  
-unset($_SESSION['ADMIN_ROLE']); 
-// 4. Destroy the session
-// session_destroy();
-redirect(web_root."admin/login.php?logout=1");
+header('Location:index.php');
 ?>
